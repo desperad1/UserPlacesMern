790bda4f620a1f865126a759c9008603c546706a
Emir Elkabes <desperad5@yahoo.com>
2022-04-25 17:37:52 +0300

    routing implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import{BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
+import Users from './User/Pages/Users';
+import NewPlace from './Places/Pages/NewPlace';
 function App() {
-  return <h1>Let's start!</h1>;
+  return <Router>
+    <Switch>
+    <Route path ="/" exact>
+      <Users/>
+    </Route>
+    <Route path="/places/new" exact>
+      <NewPlace/>
+      </Route>
+    <Redirect to="/"/>
+    </Switch>
+  </Router>
 }
 
 export default App;
