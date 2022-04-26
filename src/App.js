be990248fdc0309d3c9a48497df172959c1404e0
Emir Elkabes <desperad5@yahoo.com>
2022-04-26 15:41:21 +0300

    Changes for UsersList and UserItem components,css styling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import{BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
 import Users from './User/Pages/Users';
 import NewPlace from './Places/Pages/NewPlace';
 function App() {
-  return <Router>
+  return (
+   <Router>
     <Switch>
     <Route path ="/" exact>
       <Users/>
     </Route>
     <Route path="/places/new" exact>
       <NewPlace/>
-      </Route>
+    </Route>
     <Redirect to="/"/>
     </Switch>
   </Router>
+  )
 }
 
 export default App;
--- src/User/Pages/Users.js
+++ src/User/Pages/Users.js
@@ -1,7 +1,28 @@
 import React from "react";
+import UsersList from "../Components/UsersList"
 
-const Users=()=>{
-    return <h2>Users works</h2>;
+const Users = () => {
+    const USERS = [{
+        id: 'u1',
+        name: 'Emir Elkabes',
+        image: 'https://fastly.4sqi.net/img/user/260x260/5059923_hyrE1MTO_K1P0nD04O9WaMFvjGW-ouDcs5patTkGY8AqFG2q1Om3NiM0IpDVG4MkMU1DfD8SK',
+        places: 3
+    },{
+        image: "https://www.sezerbozkir.com/wp-content/uploads/2019/07/sezerbozkir.png",
+        id: 'u2',
+        name: 'Sezer BOZKIR',
+        places: 3
+    },
+    {
+        id:'u3',
+        name:'Mert Destici',
+        image:'https://media-exp1.licdn.com/dms/image/C4D03AQF-PAThRB5tEg/profile-displayphoto-shrink_200_200/0/1628660386739?e=1655942400&v=beta&t=bziTMOlVZTMp2otaVJbPGX9e0BbD1pSrcRgk2oz-dp4',
+        places:2
+    }
+]
+    return <UsersList items = { USERS }/>;
 }
 
 export default Users;
+
+//https://fastly.4sqi.net/img/user/260x260/5059923_hyrE1MTO_K1P0nD04O9WaMFvjGW-ouDcs5patTkGY8AqFG2q1Om3NiM0IpDVG4MkMU1DfD8SK
